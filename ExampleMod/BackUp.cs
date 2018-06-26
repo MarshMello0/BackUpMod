c8b5d88751fede5978ef77a3fcfdab8379fe0118
MarshMello <33008390+MarshMello0@users.noreply.github.com>
2018-06-26 11:23:16 +0100

    Finished Version 1

--- ExampleMod/BackUp.cs
+++ ExampleMod/BackUp.cs
@@ -20,8 +20,8 @@ public class BackUp : Mod
     int timeWait = 300; //This is in seconds and the default(300) is 5 minutes
     int amountOfBackups = 5; //This is just how many back up that it will save before it starts over riding them
     int backupNumber = 1;
-    bool idSet = false; //if they have set the ID in the config file yet or not
-    int usersID;
+    bool idSet; //if they have set the ID in the config file yet or not
+    Int64 usersID;
     bool worldLoaded = false;
     public string worldName;
 
@@ -39,28 +39,56 @@ public class BackUp : Mod
             Log("BackUp Mod: Base folder was missing so I created a new one");
             Directory.CreateDirectory("mods/BackUpMod");
         }
-
-        if (!File.Exists("mods/BackUpMod/config.txt"))
-        {
-            Log("BackUp Mod: config.txt was missing, so I created a new one");
-            File.Create("mods/BackUpMod/config.txt");
-        }
-        else
+        if (File.Exists("mods/BackUpMod/config.txt"))
         {
             StreamReader reader = new StreamReader("mods/BackUpMod/config.txt");
-            string id = reader.ReadLine();//
-            int result;
-            if (int.TryParse(id, out result))
+            string id = reader.ReadLine();
+            Int64 result;
+            if (Int64.TryParse(id, out result))
             {
                 usersID = result;
                 idSet = true;
             }
             else
             {
-                Log("BackUp Mod: In the Config.txt the user ID has been modfided and can't be loaded");
+                Log("BackUp Mod: Error loading config.txt on line 1, please re enter your ID");
                 idSet = false;
             }
+
+            string waitTimeString = reader.ReadLine();
+            int result2;
+            if (int.TryParse(waitTimeString,out result2))
+            {
+                timeWait = result2;
+            }
+            else
+            {
+                Log("BackUp Mod: Error loading config.txt on line 2, set to default");
+            }
+
+            string amountOfBackupsString = reader.ReadLine();
+            int result3;
+            if (int.TryParse(amountOfBackupsString, out result3))
+            {
+                amountOfBackups = result3;
+            }
+            else
+            {
+                Log("BackUp Mod: Error loading config.txt on line 3, set to default");
+            }
+
+            string backupNumberString = reader.ReadLine();
+            int result4;
+            if (int.TryParse(backupNumberString, out result4))
+            {
+                backupNumber = result4;
+            }
+            else
+            {
+                Log("BackUp Mod: Error loading config.txt on line 4, set to default");
+            }
             reader.Close();
+            Log("BackUp Mod: Loaded Config.txt");
         }
     }
 
@@ -86,8 +114,6 @@ public class BackUp : Mod
                 {
                     if (lastCommand.Split(' ').Length >= 2)
                     {
-                        SetTime(int.Parse(lastCommand.Split(' ')[2]));
-
                         string amount = lastCommand.Split(' ')[2];
                         int result;
                         if (int.TryParse(amount, out result))
@@ -106,8 +132,6 @@ public class BackUp : Mod
                 {
                     if (lastCommand.Split(' ').Length >= 2)
                     {
-                        SetTime(int.Parse(lastCommand.Split(' ')[2]));
-
                         string amount = lastCommand.Split(' ')[2];
                         int result;
                         if (int.TryParse(amount, out result))
@@ -125,13 +149,11 @@ public class BackUp : Mod
                 {
                     if (lastCommand.Split(' ').Length >= 2)
                     {
-                        SetTime(int.Parse(lastCommand.Split(' ')[2]));
-
                         string amount = lastCommand.Split(' ')[2];
-                        int result;
-                        if (int.TryParse(amount, out result))
+                        Int64 result;
+                        if (Int64.TryParse(amount, out result))
                         {
-                            SetAmount(result);
+                            SetupID(result);
                         }
                         else
                         {
@@ -145,7 +167,24 @@ public class BackUp : Mod
                         Log("Please put a valid id");
                     }
                 }
-                
+                else if (lastCommand.Split(' ')[1] == "now")
+                {
+                    if (lastCommand.Split(' ').Length >= 2)
+                    {
+                        BackupSave(int.Parse(lastCommand.Split(' ')[2].ToString()));
+                    }
+                    else
+                    {
+                        Log("Error");
+                        Log("Please put a valid id");
+                    }
+                }
+                else
+                {
+                    Log("Unknow Command");
+                    Log("Try 'backup help' for a list of commands");
+                }
+
             }
         }
     }
@@ -170,22 +209,22 @@ public class BackUp : Mod
 
     }
 
-    private void SetupID(int id)
+    private void SetupID(Int64 id)
     {
-        CheckBaseFiles();//Just incase some person said, LET DELETE THESE FILES AND BREAK THE MOD
-
-        File.Delete("mods/BackUpMod/config.txt");
-
-        StreamWriter writer = new StreamWriter("mods/BackUpMod/config.txt");
-
-        writer.WriteLine(id);
-        writer.WriteLine(timeWait);
-        writer.WriteLine(amountOfBackups);
-        writer.WriteLine(backupNumber);
-        writer.Close();
-
-        idSet = true;//They have set the id up
-
+        Log(id.ToString());
+        try
+        {
+            CheckBaseFiles();//Just incase some person said, LET DELETE THESE FILES AND BREAK THE MOD
+            usersID = id;
+            idSet = true;//They have set the id up
+            UpdateConfigFile();
+            Log("BackUp Mod: ID is now set to " + usersID);
+        }
+        catch (Exception e)
+        {
+            Log("BackUp Mod: Error ");
+            Log(e.ToString());
+        }
     }
     private void SetTime(int time)
     {
@@ -193,6 +232,7 @@ public class BackUp : Mod
         {
             timeWait = time;
             Log("BackUp Mod: The time between each backup has changed to " + timeWait);
+            UpdateConfigFile();
         }
         else
         {
@@ -200,13 +240,13 @@ public class BackUp : Mod
         }
 
     }
-
     private void SetAmount(int amount)
     {
         if (idSet)
         {
             amountOfBackups = amount;
             Log("BackUp Mod: Number of backups has changed to " + amountOfBackups);
+            UpdateConfigFile();
         }
         else
         {
@@ -217,19 +257,42 @@ public class BackUp : Mod
 
     private void BackupSave(int backupNumber)
     {
-        if (!idSet)
-        {
-            Log("BackUp Mod: Tried to back up and set ID is false, please contact Marsh.Mello on the forms");
-            return;
-        }
-
+        if (!worldLoaded) return;//Not to backup if in main menu
+        UpdateWorldName();
         Log("BackUp Mod: Backing Up");
+        try
+        {
+            if (!Directory.Exists("mods/BackUpMod/" + worldName))
+            {
+                Directory.CreateDirectory("mods/BackUpMod/" + worldName);
+            }
+
+            if (File.Exists("mods/BackUpMod/" + worldName + "/" + worldName + "_" + backupNumber + ".rgd"))//If its already saved, we are deleting it to override it
+            {
+                File.Delete("mods/BackUpMod/" + worldName + "/" + worldName + "_" + backupNumber + ".rgd");
+            }
+
+            UnityEngine.Object.FindObjectOfType<PauseMenu>().SaveGame();//Save to the file first
+
+            File.Copy(Directory.GetParent(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData)) + "/LocalLow/Redbeet Interactive/Raft/User/User_" + usersID + "/World/" + worldName + ".rgd", "mods/BackUpMod/" + worldName + "/" + worldName + "_" + backupNumber + ".rgd");
+
+        }
+        catch (Exception e)
+        {
+            Log(e.ToString());
+        }
+        Log("BackUp Mod: Finishd Backing up");
     }
 
     private IEnumerator Timer()
     {
         yield return new WaitForSeconds(timeWait);
-        BackupSave(backupNumber);
+        if (idSet)
+        {
+            BackupSave(backupNumber);
+        }
+        StartCoroutine(Timer());
+        
     }
 
     void NotSetUpID()
@@ -239,17 +302,41 @@ public class BackUp : Mod
     
     public void Log(string text)
     {
-        RConsole.Log(text);
+        try
+        {
+            RConsole.Log(text);
+        }
+        catch (Exception e)
+        {
+            RConsole.Log(e.ToString());
+        }
+        
+    }
+
+    private void UpdateWorldName()
+    {
+        worldName = SaveAndLoad.CurrentGameFileName;
+    }
+
+    void UpdateConfigFile()
+    {
+        UpdateWorldName();
+
+        if (File.Exists("mods/BackUpMod/config.txt")) File.Delete("mods/BackUpMod/config.txt");
+
+        StreamWriter writer = new StreamWriter("mods/BackUpMod/config.txt");
+
+        writer.WriteLine(usersID);
+        writer.WriteLine(timeWait);
+        writer.WriteLine(amountOfBackups);
+        writer.WriteLine(backupNumber);
+        writer.Close();
+
+        Log("BackUp Mod: Updated Config.txt");
     }
 
     void Update()
     {
-        if (Input.GetKey(KeyCode.F9))
-        {
-            Log(Semih_Network.GameSceneName);
-            //Log(SceneManager.GetActiveScene().name);
-        }
-
         if (SceneManager.GetActiveScene().name == "MainScene" && !worldLoaded)
         {
             worldLoaded = true;
@@ -260,15 +347,5 @@ public class BackUp : Mod
         }
     }
 
-    
-}
 
-[HarmonyPatch(typeof(RGD_Game))]
-class RGD_Game_Variable_Patch
-{
-    static void Prefix(RGD_Game __instance)
-    {
-        string worldName = (string)AccessTools.Field(typeof(RGD_Game), "name").GetValue(__instance);
-        RConsole.Log(worldName);
-    }
 }
